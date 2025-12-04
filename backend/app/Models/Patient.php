a95cbf2b8888bc1f0809d8942dabc2f8c44f964e
Laion459 <laion459@gmail.com>
2025-12-04 17:03:38 -0300

    refactor: remove unused CPF import from Patient model

--- backend/app/Models/Patient.php
+++ backend/app/Models/Patient.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use App\Domain\Shared\ValueObjects\CPF;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
